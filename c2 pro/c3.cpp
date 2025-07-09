3c6184547e8efdc10db2dd3963acfd4c76556300
Unish <unishshilpakar01@gmail.com>
2025-07-09 23:19:10 +0545

    update

---
+++ c2 pro/c3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void sound() {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+};
+
+int main() {
+    Dog d;
+    d.sound();
+    return 0;
+}
